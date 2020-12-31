1f5ac3be243763165e0d9c00aaff03af3f7c2c14
Tomal991 <intesarulhaquey4@gmail.com>
2020-12-31 23:04:34 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Kylie jenner",
+        job: "web developer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+        text:
+            "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+        id: 2,
+        name: "Hannah",
+        job: "web designer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        text:
+            "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+        id: 3,
+        name: "Peter Parker",
+        job: "Software Engineer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+        text:
+            "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+        id: 4,
+        name: "Tom Holland",
+        job: "Project manager",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text:
+            "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let current = 0;
+
+window.addEventListener('DOMContentLoaded', function () {
+    showperson(current);
+
+});
+
+function showperson(person) {
+    const item = reviews[current];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+nextBtn.addEventListener('click', function () {
+    current++;
+    if (current >= reviews.length) {
+        current = 0;
+    }
+    showperson(current);
+
+});
+
+prevBtn.addEventListener('click', function () {
+    current--;
+    if (current < 0) {
+        current = reviews.length - 1;
+    }
+    showperson(current);
+
+});
+
+randomBtn.addEventListener('click', function () {
+    current = Math.floor(Math.random() * reviews.length);
+    showperson(current);
+});
+
